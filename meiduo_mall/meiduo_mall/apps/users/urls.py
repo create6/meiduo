5b8025c4e3b0c65e9150070d0c1bed52ca63b3db
jack3306 <1362254116@qq.com>
2019-05-22 20:12:30 +0800

    modify

--- meiduo_mall/meiduo_mall/apps/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^register/$',views.RegisterView.as_view(),name="register"),
-    url(r'^usernames/(?P<username>\w{5,20})/count/$',views.CheckUsernameView.as_view(),name="username"),
-    url(r'^mobiles/(?P<mobile>1[345789]\d{9})/count/$',views.CheckMobileView.as_view(),name="mobile"),
-]
